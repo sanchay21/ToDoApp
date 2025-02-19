cff64ae7d20ae6e837dbb7c128d7f0a88917bd64
Sanchay Singh <sanchaysingh21@gmail.com>
2025-02-19 18:58:24 +0530

    API for fetching list

--- Backend/todo/models.py
+++ Backend/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todolist(models.Model):
+    priority = models.IntegerField()
+    task = models.CharField(max_length=100)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return self.task
+    
--- Backend/todo/views.py
+++ Backend/todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Todolist
+from django.core import serializers
 
 # Create your views here.
+def homepage(request):
+    return HttpResponse("Hello")
+
+def getTodoList(request):
+    # todolist = Todolist.objects.all()
+    # data = serializers.serialize('json', todolist)
+    # return HttpResponse(data, content_type='application/json')  ## Using Serializers
+    todolist = Todolist.objects.all().values("priority", "task", "status") 
+    return JsonResponse(list(todolist), safe=False)  ## Directly using json response
+    
